0e264d3d70733bc55646a808b5ee9101cc8a032a
ALUMNO <ALUMNO@DESKTOP-V0F7RQ5>
2026-03-26 18:30:41 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+
+
+
+public class Equipo
+{
+    public int codigo;
+    public string nombre;
+    public string tipo;
+    public double Usohora;
+    public int PrestadoHora;
+    public string estado;
+
+
+    public double CostoTotal()// double para calcular horas prestadas
+    {
+        return Usohora * PrestadoHora;
+
+         }
+
+  
+
+    public void MostrarDatos() // void para mostrar los datos
+    {
+        Console.WriteLine("Nombre del equipo:_ "+nombre);
+        Console.WriteLine("Tipo de equipo:_ "+tipo);
+        Console.WriteLine("Costo de uso por hora:_" + Usohora);
+        Console.WriteLine($"Total por prestamo:_ {CostoTotal:f2} ");
+        Console.WriteLine("Estado del equipo:_ "+estado);
+    }
+
+
+
+}
